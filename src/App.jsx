33ce90ba13717901f5c28a571444424d182483c6
taha arif <taha.aziz.arif@gmail.com>
2025-01-28 20:17:51 -0500

    THE TREE HATH SPROUTED LINKS HUZZAH

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Links from './components/Links'
 function App() {
 
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-800">
+    <div className="min-h-screen ">
       <Navbar />
       <Headshot />
       <Links />
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,26 +1,33 @@
-import { FaGithub, FaLinkedin, FaWordpress, FaYoutube, FaTwitch } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaWordpress, FaYoutube, FaTwitch, FaInstagram, FaDiscord, FaDollarSign, FaTiktok, FaDownload } from "react-icons/fa";
 
 function Links() {
     const links = [
-        { name: "GitHub", url: "https://github.com/yourusername", icon: <FaGithub /> },
+        { name: "View Resume", url: "/assets/files/taha_Resume_2025.pdf", icon: < FaDownload />, download: true },
+        { name: "GitHub", url: "https://github.com/tahaListens", icon: <FaGithub /> },
         { name: "Blog", url: "https://jazzbuildsthe919.wordpress.com", icon: <FaWordpress /> },
         { name: "Youtube", url: "https://www.youtube.com/@tahaListens", icon: <FaYoutube /> },
         { name: "Twitch", url: "https://www.twitch.tv/tahalistens", icon: <FaTwitch /> },
         { name: "LinkedIn", url: "https://linkedin.com/in/yourusername", icon: <FaLinkedin /> },
+        { name: "Instagram", url: "https://www.instagram.com/tahalistens", icon: <FaInstagram /> },
+        { name: "Discord", url: "https://discord.gg/kuym92hC6P", icon: <FaDiscord /> },
+        { name: "TikTok", url: "https://www.tiktok.com/@tahalistens", icon: < FaTiktok /> },
+        { name: "Donate", url: "https://paypal.me/tahaListens?country.x=US&locale.x=en_US", icon: <FaDollarSign /> }
 
 
     ];
 
     return (
         <div className="mt-8 flex flex-col items-center">
+            <a href=""></a>
+
             {links.map((link, index) => (
                 <a
                     key={index}
                     href={link.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="w-64 mb-4 py-2 flex items-center justify-center gap-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition-all"
-                >
+
+                    className="mb-3 w-56 py-2 px-4 flex items-center justify-center gap-2 
+                        bg-white text-blue-500 rounded-md shadow-sm hover:shadow-md 
+                        border border-gray-200 transition-all"                >
                     {link.icon}
                     {link.name}
                 </a>
