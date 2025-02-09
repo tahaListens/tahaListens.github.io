6111da784b082af6ff7cbac43d2fd136cdd3bb71
Hussain Arif <hussainarif@hussains-mbp.lan>
2025-02-09 01:48:11 -0500

    html.dark

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,9 @@ import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full mb-8 p-4 shadow-md bg-surface-nav drop-shadow-md dark:bg-dark-surface-nav dark:text-white">
-      <h1 className="text-xl font-bold text-center">tahaListens</h1>
+    // <nav className="fixed top-0 left-0 w-full mb-8 p-4 shadow-md bg-surface-nav drop-shadow-md dark:bg-dark-surface-nav dark:text-white">
+    <nav className="fixed top-0 left-0 w-full mb-8 p-4 shadow-md bg-surface-nav drop-shadow-md bg-surface-nav">
+      <h1 className="text-xl font-bold text-center text-text-1">tahaListens</h1>
       <div className="absolute right-4 top-1/2 transform -translate-y-1/2">
         <ThemeToggle />
       </div>
