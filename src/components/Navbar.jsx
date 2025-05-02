c5357d4535ef8815f6ba7cc953d674a0ded719ba
taha arif <taha.aziz.arif@gmail.com>
2025-05-02 00:26:49 -0400

    Adjusted particle background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   return (
     // <nav className="fixed top-0 left-0 w-full mb-8 p-4 shadow-md bg-surface-nav drop-shadow-md dark:bg-dark-surface-nav dark:text-white">
-    <nav className="fixed top-0 left-0 w-full mb-8 p-4 shadow-md bg-surface-nav drop-shadow-md bg-surface-nav">
+    <nav className="fixed top-0 left-0 w-full mb-8 p-4 shadow-md bg-surface-nav drop-shadow-md">
       <h1 className="text-xl font-bold text-center text-text-1">tahaListens</h1>
       <div className="absolute right-4 top-1/2 transform -translate-y-1/2">
         <ThemeToggle />
