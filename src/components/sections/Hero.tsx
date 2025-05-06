0002e849dcd6eb16ba2ec8909720b3aa35a4fde6
taha arif <taha.aziz.arif@gmail.com>
2025-05-06 13:54:26 -0400

    added hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -39,21 +39,25 @@ export const Hero = () => {
               <form
                 action="#"
                 className="py-1 pl-6 w-full pr-1 flex gap-3 items-center text-heading-3
-                                 shadow-lg shadow-box-shadow border border-box-border bg-box-bg rounded-full
-                                 ease-linear focus-within:bg-body focus-within:border-primary"
+                                          shadow-lg shadow-box-shadow border border-box-border
+                                          bg-box-bg rounded-full ease-linear focus-within:bg-body
+                                          focus-within:border-primary"
               >
                 <span className="min-w-max pr-2 border-r border-box-border">
+                  {" "}
                   <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
                     xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    stroke="currentColor"
+                    width="20"
+                    fill="none"
+                    viewBox="0 0 24 24"
                     strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5"
                   >
                     <path
                       strokeLinecap="round"
-                      d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z"
+                      strokeLinejoin="round"
+                      d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
                     />
                   </svg>
                 </span>
@@ -63,7 +67,7 @@ export const Hero = () => {
                   className="w-full py-3 outline-none bg-transparent"
                 />
                 <Button className="min-w-max text-white">
-                  <span className=" z-[5]"> Get Started!</span>
+                  <span className="relative z-[5]">Get Started</span>
                 </Button>
               </form>
             </div>
@@ -71,17 +75,13 @@ export const Hero = () => {
         </div>
         {/* Hero Image */}
 
-        <div
-          className="flex flex-1  lg:w-1/2 lg:h-200 relative lg:max-w-none lg:mx-0
-                            mx-auto max-w-3xl"
-        >
+        <div className="flex flex-1 lg:w-1/2 lg:h-auto relative lg:max-w-non lg:mx-0 mx-auto max-w-3xl">
           <img
-            src="https://images.pexels.com/photos/28180384/pexels-photo-28180384/free-photo-of-a-close-up-of-a-chinese-juniper-tree-branches-texture-pattern.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt="Hero Image"
+            src="https://images.pexels.com/photos/7773731/pexels-photo-7773731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Hero image"
             width={2350}
             height={2359}
-            className="lg:absolute lg:w-full lg:h-full rounded-3xl 
-                        object-cover lg:max-h-none max-h-96 my-5 z-10"
+            className="lg:absolute lg:w-full lg:h-full rounded-3xl object-cover lg:max-h-non max-h-96"
           />
         </div>
       </Container>
