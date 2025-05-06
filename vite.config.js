e9c5d5cfdde55f774a04487f73cbf70586ccc27c
taha arif <taha.aziz.arif@gmail.com>
2025-05-06 15:03:04 -0400

    removed old files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "docs",
+  },
   plugins: [react(), tailwindcss()],
 });
