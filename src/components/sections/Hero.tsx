2382d2a82bd3b1ea8298cad8624d4bf66fc19437
taha arif <taha.aziz.arif@gmail.com>
2025-05-06 20:58:02 -0400

    fixed Container

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,13 +4,15 @@ import { Paragraph } from "../shared/Paragraph";
 
 export const Hero = () => {
   return (
-    <section className="relative pt-32 lg:pt-36 bg-body">
+    <section className="relative pt-32 lg:pt-36 ">
+      {""}
+
       <Container className="flex flex-col lg:flex-row gap-10 lg:gap-12">
-        <div className="absolute w-full lg:w-1/2 inset-y-0 lg:right-0">
+        <div className="absolute w-full lg:w-1/2 inset-y-0 lg:left-0">
           <span
             className="absolute -left-6 md:left-4 top-24 lg:top-28 w-24 h-24 rotate-90 skew-x-12 
-                        rounded-3xl bg-gradient-to-r from-blue-600 to-violet-600 blur-xl opacity-60     
-                        lg:opacity-95 lg:block-hidden"
+                        rounded-3xl bg-gradient-to-r from-blue-600 to-violet-600 blur-xl opacity-60
+                        lg:opacity-95 lg:block"
           ></span>
           <span
             className="absolute right-4 bottom-12 w-24 h-24 rounded-3xl bg-primary blur-xl 
@@ -19,8 +21,8 @@ export const Hero = () => {
         </div>
 
         <div
-          className="relative flex flex-col align-center text-center lg:text-left lg:py-8 lg:items-start
-                     lg:max-w-none max-w-3xl mx-auto lg:mx-0 lg:flex-1 lg:w-1/2"
+          className="relative flex flex-col items-center text-center lg:text-left lg:py-8 
+                    lg:items-start lg:max-w-none max-w-3xl mx-auto lg:mx-0 lg:flex-1 lg:w-1/2"
         >
           <h1 className="text-heading-1 text-3xl sm:text-4xl md:text-5xl xl:text-6xl font-bold">
             Modern webpages for{" "}
@@ -73,15 +75,16 @@ export const Hero = () => {
             </div>
           </div>
         </div>
+
         {/* Hero Image */}
 
-        <div className="flex flex-1 lg:w-1/2 lg:h-auto relative lg:max-w-non lg:mx-0 mx-auto max-w-3xl">
+        <div className="flex flex-1 lg:w-1/2 min-h-30 lg:h-auto relative lg:max-w-none lg:mx-0 mx-auto max-w-3xl">
           <img
             src="https://images.pexels.com/photos/7773731/pexels-photo-7773731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
             alt="Hero image"
             width={2350}
             height={2359}
-            className="lg:absolute lg:w-full lg:h-full rounded-3xl object-cover lg:max-h-non max-h-96"
+            className="lg:absolute lg:w-full lg:h-full rounded-3xl object-cover lg:max-h-none max-h-96"
           />
         </div>
       </Container>
